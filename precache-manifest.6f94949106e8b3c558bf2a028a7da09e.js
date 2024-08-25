041a1f15f838742fe70fbc2d054d1eaaed13f6f2
Randy Torres <rltorres26+github@gmail.com>
2024-08-24 20:59:51 -0500

    Updates

---
+++ precache-manifest.6f94949106e8b3c558bf2a028a7da09e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f814a520eb122cdf2b4d16a7ad69dcd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e3e1c6e72f3cd205a3f",
+    "url": "/static/css/main.dac41c0e.chunk.css"
+  },
+  {
+    "revision": "12bb844e9dbedfe3adad",
+    "url": "/static/js/2.86bcaaf8.chunk.js"
+  },
+  {
+    "revision": "9e3e1c6e72f3cd205a3f",
+    "url": "/static/js/main.01364701.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
